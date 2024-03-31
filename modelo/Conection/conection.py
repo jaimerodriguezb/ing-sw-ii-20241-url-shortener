d2fe447d74aee272b4cb56a5f8499e2b9efbbff4
maicol Ascencio <mascencio63@uan.edu.co>
2024-03-31 16:52:27 -0500

    Primer Intento de añadir el modelo

--- modelo/Conection/conection.py
+++ modelo/Conection/conection.py
@@ -1,17 +1,30 @@
+
 import requests
 from datetime import datetime, timezone
 import json
 
 TOKEN_FOR_NOTION = "secret_PEF343ZLjwQiHRqomIL67F1gaCuo4iksw7sYoK3ZxFn"
-TABLE_URL_FOR_USERS = "fa4cf4991442476798e83063c5983fc0"
-TABLE_USER = "7ab0e7992dff4e77a8501642636730d5"
-TABLE_DETECTED_PSH_URL = "1b70a4030dc34db891b8a6845a9e0fa3"
+TABLE_URL_FOR_USERS="fa4cf4991442476798e83063c5983fc0" 
+TABLE_USER="7ab0e7992dff4e77a8501642636730d5" 
+TABLE_DETECTED_PSH_URL="1b70a4030dc34db891b8a6845a9e0fa3"
 
 headers = {
     "Authorization": "Bearer " + TOKEN_FOR_NOTION,
     "Content-Type": "application/json",
     "Notion-Version": "2022-06-28",
 }
+def safe_get(data, dot_chained_keys):
+
+    keys = dot_chained_keys.split('.')
+    for key in keys:
+        try:
+            if isinstance(data, list):
+                data = data[int(key)]
+            else:
+                data = data[key]
+        except (KeyError, TypeError, IndexError):
+            return None
+    return data
 
 def Consult_data_from_TABLE_URL_FOR_USERS():
     url = f"https://api.notion.com/v1/databases/{TABLE_URL_FOR_USERS}/query"
@@ -23,7 +36,8 @@ def Consult_data_from_TABLE_URL_FOR_USERS():
     with open('db.json', 'w', encoding='utf8') as f:
         json.dump (data, f, ensure_ascii=False, indent=4)
     
-    results = data["results"]
+    results = data['results']
+   
     return results 
 
 def Consult_data_from_TABLE_USER():
@@ -37,6 +51,14 @@ def Consult_data_from_TABLE_USER():
         json.dump (data, f, ensure_ascii=False, indent=4)
     
     results = data["results"]
+    datos_conseguidos=[]
+
+    for entry in results:
+        columna_correo = safe_get(entry, 'properties.correo.rich_text.text.content')
+        columna_nombre_Usuario = safe_get(entry, 'properties.nombre_Usuario.text.content')
+        columna_password = safe_get(entry, 'properties.password.rich_text.text.content')
+        datos_conseguidos.append((columna_correo, columna_nombre_Usuario, columna_password))	
+    print (datos_conseguidos)   
     return results 
 
 def Consult_data_from_TABLE_DETECTED_PSH_URL():
@@ -52,9 +74,4 @@ def Consult_data_from_TABLE_DETECTED_PSH_URL():
     results = data["results"]
     return results 
 
-'''test = Consult_data_from_TABLE_USER
-for page in test:
-   idurl=page["nombre_Usuario"]
-   url= page["password"]
-   print(idurl, url)
-'''
+test = Consult_data_from_TABLE_USER()
