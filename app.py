2e02b47043e2b01427e3bcf900a5c35962a2e1e2
Darkpand1811d <luisalejandro14122002@gmail.com>
2024-04-30 19:35:16 -0500

    Commit para agregar la api

--- app.py
+++ app.py
@@ -1,51 +1,35 @@
-#!/usr/bin/python
-
 from flask import Flask
 from flask_restx import Api, Resource, fields
 from flask_cors import CORS
-from model.logic_gate_model_deployment import Logic
+from modelo.model import AcortadorUrl
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes and origins
-logic = Logic()
+acortador = AcortadorUrl ()
 
 api = Api(
     app, 
     version='1.0', 
-    title='Logic Gates Predictor',
-    description='Logic Gates Predictor')
+    title='Url Shortener',
+    description='url shortener with pshishing detection')
 
-ns = api.namespace('logic')
+ns = api.namespace('url')
    
 parser = api.parser()
 
 parser.add_argument(
-    'a', 
-    type=int, 
-    required=True, 
-    help='a operand', 
-    location='args')
-
-parser.add_argument(
-    'b', 
-    type=int, 
-    required=True, 
-    help='b operand', 
-    location='args')
-
-parser.add_argument(
-    'gate', 
+    'url', 
     type=str, 
     required=True, 
-    help='operator: or, and, nor, nand, xor', 
+    help='URL to short', 
     location='args')
 
 resource_fields = api.model('Resource', {
     'result': fields.Integer,
 })
 
-@ns.route('/gates')
-class LogicGategApi(Resource):
+@ns.route('/shorten')
+class Acortar(Resource):
 
     @api.doc(parser=parser)
     @api.marshal_with(resource_fields)
@@ -53,7 +37,7 @@ class LogicGategApi(Resource):
         args = parser.parse_args()
 
         return {
-         "result": logic.logic_gate(args['a'], args['b'], args['gate'])
+         "result": acortador.verificar_pishing(args['url'])
         }, 200
     
     
